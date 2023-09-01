490021d683f0a02e589e303b3e69032b42470a5f
LAPTOP-768SG1IC\pckas <2016sccsayakakira@gmail.com>
2023-09-01 17:48:08 +0900

    RegisterController.phpのバリデーション機能修正しました。

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -49,18 +49,11 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'username' => 'required|string|min:4|max:12',
-            'email' => 'required|string|email|min:4|max:12|unique:users',
-            'password' => 'required|string|alpha_num|min:4|max:12|confirmed',
-            'password-confirm' => 'required|string|alpha_num|min:4|max:12|confirmed'
-        ],
-    [
-        'username.required'=>'ユーザー名の入力は必須です',
-        'mail.required'=>'メールアドレスの入力は必須です'
-    ]);
-
-
-}
+            'username' => 'required|string|max:255',
+            'mail' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:4|confirmed',
+        ])->validate();
+    }
 
     /**
      * Create a new user instance after a valid registration.
@@ -85,7 +78,7 @@ class RegisterController extends Controller
     public function register(Request $request){
         if($request->isMethod('post')){
             $data = $request->input();
-
+            $this->validator($data);
             $this->create($data);
             return redirect('added');
         }
